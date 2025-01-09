7b0893d500066a83f03082cf05cf406dae04c053
curtindoavida <202490+curtindoavida@users.noreply.github.com>
2025-01-09 16:50:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Defina o URL do executável
 ZIP_URL="https://nirooh-instalador.github.io/nirooh.zip"
 ZIP_URL="nirooh.zip"
 EXECUTABLE_NAME="nirooh"
 INSTALL_DIR="/usr/local/bin"
 CRON_JOB="*/15 * * * * $INSTALL_DIR/$EXECUTABLE_NAME"
 
-# Função para verificar se o usuário é root
 check_root() {
     if [[ $EUID -ne 0 ]]; then
         echo "Este script precisa ser executado como root. Use sudo." >&2
@@ -15,7 +13,6 @@ check_root() {
     fi
 }
 
-# Verificar e instalar dependências (unzip)
 install_dependencies() {
     if ! command -v unzip &>/dev/null; then
         echo "Instalando a dependência 'unzip'..."
@@ -27,7 +24,6 @@ install_dependencies() {
     fi
 }
 
-# Baixar o arquivo zip
 download_zip() {
     echo "Baixando o arquivo zip de $ZIP_URL..."
     curl -o "/tmp/$ZIP_NAME" "$ZIP_URL"
@@ -38,7 +34,6 @@ download_zip() {
     echo "Arquivo zip baixado em /tmp/$ZIP_NAME."
 }
 
-# Descompactar o arquivo zip
 unzip_executable() {
     echo "Descompactando o arquivo zip..."
     unzip -o "/tmp/$ZIP_NAME" -d "/tmp/"
@@ -55,7 +50,6 @@ unzip_executable() {
     echo "Executável instalado em $INSTALL_DIR."
 }
 
-# Configurar o cron
 setup_cron() {
     echo "Configurando o crontab para rodar a cada 15 minutos..."
     (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
@@ -67,7 +61,6 @@ setup_cron() {
     fi
 }
 
-# Execução principal
 main() {
     check_root
     install_dependencies
